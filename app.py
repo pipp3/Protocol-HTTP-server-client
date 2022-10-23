2cc84ba89c10965469a3fff7086383948ebc623a
Cobresalito <116470096+Cobresalito@users.noreply.github.com>
2022-10-22 22:45:35 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ import urllib3
 
 http = urllib3.PoolManager()
 
-r = http.request('GET', 'http://httpbin.org/robots.txt')
-r.data
+r = http.request('GET', 'http://127.0.0.1:8080/Jinx_Arcane.jpg')
 
-r = http.request(
-    'POST',
-    'http://httpbin.org/post',
-    fields={'hello': 'world'}
-)
+
